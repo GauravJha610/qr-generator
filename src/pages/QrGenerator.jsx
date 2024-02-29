4bb9f12d6f077a07329aa86f904b345979c2c7ef
Gaurav Jha <gaurav.jha.cse24@heritageit.edu.in>
2024-02-29 06:53:05 +0530

    max width set

--- src/pages/QrGenerator.jsx
+++ src/pages/QrGenerator.jsx
@@ -15,7 +15,7 @@ const QrGenerator = () => {
     }
     return (
         <div className=" flex flex-col justify-center items-center h-screen ">
-            <div className=" bg-white flex flex-col justify-center  border py-4 px-8 rounded-lg ">
+            <div className=" bg-white flex flex-col justify-center  border py-4 px-8 rounded-lg max-w-[90%]">
                 <p className=" text-[20px] bg-white mt-2 mb-3">Enter text or URL</p>
                 <input type="text" placeholder="Text or URL" className=" bg-white text-black placeholder:text-black border border-black h-10 px-3 rounded-md" id="inputText"/>
                 {image_url!=="/" && 
