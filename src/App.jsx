fc02d6b504b3314b1f51e212bdb07a2fc23ed0c5
Gaurav Jha <gaurav.jha.cse24@heritageit.edu.in>
2024-02-29 06:45:11 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import QrGenerator from "./pages/QrGenerator";
+
+const App = () => {
+  console.log()
+  return (
+    <QrGenerator/>
+  );
+};
+
+export default App;
---
+++ src/pages/QrGenerator.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+const QrGenerator = () => {
+    const [image_url,setImage_url]= useState("/");
+    
+    function generateQr(){
+        let inputText=document.getElementById('inputText');
+        if(inputText.value.length>0){
+            let newUrl="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+inputText.value;
+            setImage_url(newUrl);
+        }
+        else{
+            return 0;
+        }
+    }
+    return (
+        <div className=" flex flex-col justify-center items-center h-screen ">
+            <div className=" bg-white flex flex-col justify-center  border py-4 px-8 rounded-lg ">
+                <p className=" text-[20px] bg-white mt-2 mb-3">Enter text or URL</p>
+                <input type="text" placeholder="Text or URL" className=" bg-white text-black placeholder:text-black border border-black h-10 px-3 rounded-md" id="inputText"/>
+                {image_url!=="/" && 
+                <div className="flex items-center justify-center bg-white pt-8 pb-5">
+                    <img src={image_url} alt="" className=" "/>
+                </div>
+                }
+
+                <button className=" border mt-5 mb-3 h-10 rounded-xl bg-blue-900 text-white" onClick={()=>generateQr()}>Generate QR Code</button>
+            </div>
+        </div>
+    );
+};
+
+export default QrGenerator;
